ebb2c5684a7bdab2af69499a1bbad5e79f7b5e75
Sławomir Śpiewak <slawekwaga@gmail.com>
2021-09-20 13:33:45 +0200

    include directive finished

--- include/response_system.h
+++ include/response_system.h
@@ -4,11 +4,23 @@
 
 namespace RRParser
 {
+class CResponseRulesScript;
 
 extern std::vector<std::string> entryPointsToParse;
+extern std::vector<CResponseRulesScript> responseSystems;
 
-    void initRules(std::string gameDir);
-    void dumpSceneNames();
-    void recursiveIncludePass(std::ifstream& file);
+
+void initRules(std::string gameDir); //start parsing scripts
+class CResponseRulesScript { //Contains the entry point and its include akin to CDefaultResponseSystem and CInstancedResponseSystem
+private:
+    std::vector<std::string> includedFiles;
+    static void stripQuotes(std::string& quoted);
+public:
+    bool isGlobal; //true if it's a global talker script (CDefault), false otherwise (CInstanced).
+
+    void parseScript(std::ifstream& file);
+};
+
+void dumpSceneNames(); // Iterate over scripts and extract scene filenames
 };
 
