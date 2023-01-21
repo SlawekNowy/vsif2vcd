8fc0e1dfd5930c8d68db98f0a51280e72ba54a9d
FlaminSarge <flaminsarge@gmail.com>
2023-01-20 17:24:33 -0800

    Prevent crash caused by attempting to close a null(?) file

--- src/Vsif2vcd_c++.cpp
+++ src/Vsif2vcd_c++.cpp
@@ -155,7 +155,7 @@
                     fclose(fileTest);
                     continue;
                   }
-                 fclose(fileTest);
+                 //fclose(fileTest);
             } else {
                 SPDLOG_INFO("CRC ({0:#08X}) miss!",entry.CRC);
                 std::string targetFile = "/_failed/"+std::to_string(entry.CRC)+".vcd";
